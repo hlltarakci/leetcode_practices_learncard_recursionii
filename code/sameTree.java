af7f2e338f0be704387a23d2f7035f4b673feb9c
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-20 10:40:51 +0300

    Create sameTree.java

---
+++ code/sameTree.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/explore/learn/card/recursion-ii/503/recursion-to-iteration/2894/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /*
+        p, q: nodes on p and q
+        
+        time: O(min(p,q))
+        space: O(min(h(p), h(q))) -- h will be log p for balanced, p for unbalanced
+    */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
